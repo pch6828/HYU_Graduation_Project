d77cdec4253eac514bd6e04490d358f5b20d68d2
pch6828 <pch6828@hanyang.ac.kr>
2021-09-08 23:12:20 +0900

    experiment-setup: automated compilation success

--- experiment/compile.sh
+++ experiment/compile.sh
@@ -1,7 +1,10 @@
 cd ../rocksdb-6.15.5
+make clean
 make -j10 LIBNAME=../experiment/origin_rocksdb
+cd ../experiment
+make test_with_origin_rocksdb -j10
+cd ../rocksdb-6.15.5
 make clean
 make -j10 LIBNAME=../experiment/custom_rocksdb
-make clean
 cd ../experiment
-make -j10
+make test_with_custom_rocksdb -j10
